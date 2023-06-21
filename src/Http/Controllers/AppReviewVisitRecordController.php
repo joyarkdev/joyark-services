de05ab47fff58308c489b0f6757de2c494f947d6
Victor Tang <tanghengzhi@live.com>
2023-06-21 16:02:52 +0800

    fix issues

--- src/Http/Controllers/AppReviewVisitRecordController.php
+++ src/Http/Controllers/AppReviewVisitRecordController.php
@@ -30,15 +30,23 @@ class AppReviewVisitRecordController extends Controller
 
         $ipRecord = AppReviewVisitRecord::updateOrCreate(
             ['type' => AppReviewVisitRecordType::IP, 'value' => $request->ip()],
-            ['app_id' => $appId, 'version' => $version, 'visit_time' => now(), 'visit_count' => 0]
+            ['app_id' => $appId, 'version' => $version, 'visit_time' => now(), 'visit_count' => 1]
         );
-        $ipRecord->increment('visit_count', 1);
+
+        if (! $ipRecord->wasRecentlyCreated) {
+            $ipRecord->increment('visit_count', 1);
+            $ipRecord->save();
+        }
 
         $deviceIdRecord = AppReviewVisitRecord::updateOrCreate(
             ['type' => AppReviewVisitRecordType::DEVICE_ID, 'value' => $request->header('device-id')],
-            ['app_id' => $appId, 'version' => $version, 'visit_time' => now(), 'visit_count' => 0]
+            ['app_id' => $appId, 'version' => $version, 'visit_time' => now(), 'visit_count' => 1]
         );
-        $deviceIdRecord->increment('visit_count', 1);
+
+        if (! $deviceIdRecord->wasRecentlyCreated) {
+            $deviceIdRecord->increment('visit_count', 1);
+            $deviceIdRecord->save();
+        }
 
         return response()->noContent();
     }
